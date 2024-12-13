d5126de29908579fcab4454ec210298b8af52298
Otavio <otavio.zerotwo@gmail.com>
2024-12-12 21:29:02 -0300

    feat: add img user profile + animações

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 const navbar = document.getElementById('navbar');
+const imgUser = document.getElementById('imgUser');
 
 const navbarOffsetTop = navbar.offsetTop;
 
 window.addEventListener('scroll', () => {
   if (window.scrollY >= navbarOffsetTop) {
     navbar.classList.add('navbar-fixed');
+    imgUser.classList.add('img-user-size-custom');
+    imgUser.classList.remove('border-5');
   } else {
     navbar.classList.remove('navbar-fixed');
+    imgUser.classList.remove('img-user-size-custom');
+    imgUser.classList.add('border-5');
   }
 });
